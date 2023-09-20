ba579b2392b342ff2b4dd16e65b5a5c58a589658
grigoryan.m <waldm4rk@gmail.com>
2023-09-20 11:19:24 +0400

    first commit

---
+++ src/Panel.js
@@ -0,0 +1,14 @@
+import './Panel.css';
+import React from 'react';
+
+function Panel(){
+    return(
+        <div className='wrapper'>
+            
+        </div>
+
+
+    )
+}
+
+export default Panel;
