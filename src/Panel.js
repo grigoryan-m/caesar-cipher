e615f3985649fa8503016824351f6dd3ee7a293b
grigoryan.m <waldm4rk@gmail.com>
2023-09-20 11:40:17 +0400

    finished project

--- src/Panel.js
+++ src/Panel.js
@@ -1,10 +1,55 @@
 import './Panel.css';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Panel(){
+    useEffect(() => {
+
+        const plainText = document.getElementById("plainText")
+        ,     cipherText = document.getElementById("cipherText");
+
+        const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        plainText.addEventListener("input", ()=>{
+            let cipher = '';
+
+            for(let i = 0; i < plainText.value.length; i++){
+                if(plainText.value.charAt(i) == 'z'){
+                    cipher += 'a';
+                }else{
+                    if(alphabet.includes(plainText.value.charAt(i))){
+                        cipher += alphabet[alphabet.indexOf(plainText.value.charAt(i)) + 1];
+                    }else{
+                        cipher += plainText.value.charAt(i);
+                    }
+                }
+            }
+            cipherText.value = cipher;
+        });
+
+        cipherText.addEventListener("input", ()=>{
+            let text = '';
+
+            for(let i = 0; i < cipherText.value.length; i++){
+                if(cipherText.value.charAt(i) == 'a'){
+                    text += 'z';
+                }else{
+                    if(alphabet.includes(cipherText.value.charAt(i))){
+                        text += alphabet[alphabet.indexOf(cipherText.value.charAt(i)) - 1];
+                    }else{
+                        text += cipherText.value.charAt(i);
+                    }
+                }
+            }
+            plainText.value = text;
+        });
+    });
+
     return(
-        <div className='wrapper'>
-            
+        <div className='Wrapper'>
+            <main className='Panel'>
+                <input id="plainText" placeholder='Type text here...'/>
+                <input id="cipherText" placeholder='Type cipher here...' />
+            </main>
         </div>
 
 
